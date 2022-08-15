5d842cce415542d21f6d46ace5cb46e32fd9be4a
Lumière <37071009+Jaime0@users.noreply.github.com>
2022-08-15 09:53:04 +0800

    Create TimeTools.py
    This py file contains the functions to resolve the time (transfer)

---
+++ TimeTools.py
@@ -0,0 +1,9 @@
+import time
+import calendar
+
+str_time = '2022-08-15T01:25:29.126869+00:00'
+refactor_time = time.strptime(str(str_time.replace('T', ' ')[:-6]), "%Y-%m-%d %H:%M:%S.%f") # just remain seconds
+second = calendar.timegm(refactor_time)
+mill_seconds = str_time[-12:-6]
+time_with_ms = float(str(second) + "." + mill_seconds)
+print(time_with_ms)
