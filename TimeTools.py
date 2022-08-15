1c4535cf6bdbba38d50ac9626ac2de40c8282f60
Lumière <37071009+Jaime0@users.noreply.github.com>
2022-08-15 09:53:57 +0800

    Update TimeTools.py

--- TimeTools.py
+++ TimeTools.py
@@ -1,6 +1,7 @@
 import time
 import calendar
 
+# 处理UTC时间到指定的字符串
 str_time = '2022-08-15T01:25:29.126869+00:00'
 refactor_time = time.strptime(str(str_time.replace('T', ' ')[:-6]), "%Y-%m-%d %H:%M:%S.%f") # just remain seconds
 second = calendar.timegm(refactor_time)
